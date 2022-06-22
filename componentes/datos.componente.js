0be84740e26caf52ab6c91fd0d27ca29c1a45371
JuDaSe <juandasegundo@gmail.com>
2022-06-22 11:09:45 -0300

    Aplicacion ABM  ACTUALIZADA

--- componentes/datos.componente.js
+++ componentes/datos.componente.js
@@ -1,11 +1,10 @@
-const API   = 'https://crudcrud.com/api/4f21865f7a014a81937f18f564786846'
+const API   = 'https://crudcrud.com/api/aa2807a49a7343c081ebaa2dfc43b2eb'
 const model = '/personas'
 
-const Productos = {
+const Datos = {
     todos: function (cb){
         const url = API + model
         axios.get(url).then( response => {
-            // Me devuelve una funcion que tiene un array
             cb(response.data)
         }).catch( err => {
             cb({
@@ -26,7 +25,6 @@ const Productos = {
     eliminar: function (id, cb){
         const url = API + model + '/' + id
         axios.delete(url).then( response => {
-            // Me devuelve una funcion que tiene un array
             cb(response.data)
         }).catch( err => {
             cb({
@@ -49,24 +47,24 @@ const CrudDatos = Vue.component('datos-componente', {
     data: function (){
         return {
             persona: {},
-            items: []
+            items: [],
         }
     },
     methods: {
         guardarItem: function (){
-            if(!this.persona._id){
-                Productos.crear(this.persona, response => {
+            if(!this.persona._id)
+            {  
+                Datos.crear(this.persona,  response => {
                     if(!response.error){
                         this.cargaItems()
                     }
                 })
             } else {
-                // Si me llega la _id edito
                 let id      = this.persona._id
                 let persona = {...this.persona}
                 delete persona._id
 
-                Productos.editar(id, persona, response => {
+                Datos.editar(id, persona, response => {
                     if(!response.error){
                         this.cargaItems()
                     }
@@ -75,7 +73,7 @@ const CrudDatos = Vue.component('datos-componente', {
             
         },
         cargaItems: function (){ 
-            Productos.todos( response => {
+            Datos.todos( response => {
                 if(!response.error){
                     this.items = response
                     this.persona = {}
@@ -84,7 +82,7 @@ const CrudDatos = Vue.component('datos-componente', {
             })
         },
         eliminarItem: function (_id){ 
-            Productos.eliminar(_id, response => {
+            Datos.eliminar(_id, response => {
                 if(!response.error){
                     this.cargaItems()
                 }
@@ -92,42 +90,84 @@ const CrudDatos = Vue.component('datos-componente', {
         },
         editarItem: function (obj){
             this.persona = {...obj}
-        }
+        },
+        obtenerFecha(){
+            const date = new Date();
+            return this.fecha = date.toLocaleDateString()
+        },
     },
-    mounted: function (){
-        console.log("APP lista :: ")
+    mounted: function(){
         this.cargaItems()
+        this.obtenerFecha()
     },
     template: `
      <div id="datos-componente">
-        <input type="text" v-model="persona.nombre" placeholder="Nombre">
-        <input type="text" v-model="persona.apellido" placeholder="Apellido">
-        <input type="number" v-model="persona.edad">
-        <button @click="guardarItem()">Agregar</button>
-        <hr>
-        <ul>
-            <li v-for="(itm, index) in items" :key="index">
-                <span>{{itm.nombre}} {{itm.apellido}} {{itm.edad}}</span>
-                <button @click="eliminarItem(itm._id)">Eliminar</button>
-                <button @click="editarItem(itm)">Editar</button>
-            </li>
-        </ul>
+     <form>
+
+    <label for="Nombre">Nombre</label>
+    <input type="text" id="name" value="Nombre" v-model="persona.nombre">
+
+    <label for="Apellido">Apellido</label>
+    <input type="text" id="apellido" value="Apellido" v-model="persona.apellido">
+
+    <label for="edad">Edad</label>
+    <input type="number" id="edad" value="edad" v-model="persona.edad">
+
+    <label for="Contraseña">Contraseña</label>
+    <input type="password" id="contraseña" value="contraseña" v-model="persona.pass">
+
+    <label for="Correo">Correo</label>
+    <input type="email" id="correo" value="correo" v-model="persona.email">
+
+    <label for="Genero">Genero</label>
+    <select value="Genero "v-model="persona.genero">
+        <option >Masculino</option>
+        <option >Femenino</option>
+        <option >Otro</option>
+    </select>
+
+    <label for="Estado">Activo</label>
+    <select value="Estado" v-model="persona.activo">
+        <option selected>Sí</option>
+        <option>No</option>
+    </select>
+
+    <label for="Altura">Altura</label>
+    <input type="number" id="contraseña" value="contraseña" v-model="persona.altura">
+    </form>
+    
+     <button @click="guardarItem()">Agregar</button>
+     <hr>
+     <ul>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+                <th>Contraseña</th> 
+                <th>Correo</th> 
+                <th>Genero</th> 
+                <th>Altura</th> 
+                <th>Activo</th> 
+                <th>Fecha Creación</th>
+            </tr>
+        </thead>
+        <table>
+         <li v-for="(itm, index) in items" :key="index">
+              <td>{{itm.nombre}}
+              <td>{{itm.apellido}}</td>
+              <td>{{itm.edad}}</td> 
+              <td>{{itm.pass}}</td> 
+              <td>{{itm.email}}</td> 
+              <td>{{itm.genero}}</td> 
+              <td>{{itm.altura}} CM</td> 
+              <td>{{itm.activo}}</td> 
+              <td>{{obtenerFecha()}}</td>
+              <td> <button @click="eliminarItem(itm._id)">Eliminar</button></td>
+              <td><button @click="editarItem(itm)">Editar</button></td>
+            </table>
+         </li>
+     </ul>
      </div>
     `
-
-    // <div id="datos-persona-component">
-    //         <input type="text" v-model="nombre" placeholder="Nombre">
-    //         <input type="text" v-model="apellido" placeholder="Apellido">
-    //         <input type="number" v-model="edad" placeholder="Edad">
-    //         <input type="email" v-model="email" placeholder="Correo">
-    //         <button @click="guardarItem()">Agregar</button>
-    //         <hr>
-    //         <ul>
-    //             <li v-for="(itm) in items" :key="index">
-    //                 <span>{{itm.nombre}} {{itm.apellido}} | Edad: {{itm.edad}} | Correo: {{itm.email}}</span>
-    //                 <button @click="eliminarItem(itm._id)">Eliminar</button>
-    //                 <button @click="editarItem(itm)">Editar</button>
-    //             </li>
-    //         </ul>
-    //     </div>
 })
